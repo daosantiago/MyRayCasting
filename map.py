23dbcc766fbb8836c1ad3f1c9d83ad3ed3fd2b1b
Anderson <daosantiago@gmail.com>
2023-03-14 21:20:24 +0000

    create a matrix, bind to a map and draw it

---
+++ map.py
@@ -0,0 +1,41 @@
+import pygame as pg
+
+from settings import *
+
+matrix = [
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+]
+
+
+class Map:
+    def __init__(self, game) -> None:
+        self.game = game
+        self.matrix = matrix
+        self.map = {}
+        self.get_map()
+
+    def get_map(self):
+        for j, row in enumerate(self.matrix):
+            for i, value in enumerate(row):
+                if value:
+                    # stores only the cells to be draw
+                    self.map[(i, j)] = value
+
+    def draw(self):
+        for pos in self.map:
+            pg.draw.rect(self.game.screen, 'red', (pos[0] * TILE_SIZE,
+                                                   pos[1] * TILE_SIZE, TILE_SIZE, TILE_SIZE), 2)

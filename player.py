9bb61f915efaff868acaac1d0a3bf672073454cd
Anderson <daosantiago@gmail.com>
2023-03-14 21:40:29 +0000

    create methods for user

--- player.py
+++ player.py
@@ -10,3 +10,21 @@ class Player:
         self.game = game
         self.x, self.y = PLAYER_POS
         self.angle = PLAYER_ANGLE
+
+
+def move(self):
+    pass
+
+
+def update(self):
+    pass
+
+
+@property
+def pos(self):
+    return self.x, self.y
+
+
+@property
+def map_pos(self):
+    return int(self.x), int(self.y)

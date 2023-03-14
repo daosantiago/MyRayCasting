2f235db9010bcd27e3b1241344e6d592d0a884f2
Anderson <daosantiago@gmail.com>
2023-03-14 22:06:50 +0000

    calls player in main

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import sys
 
 from map import *
+from player import *
 from settings import *
 
 
-class App:
+class Game:
     def __init__(self):
         pg.init()
         pg.display.set_caption('My RayCasting')
@@ -12,13 +13,16 @@ class App:
         self.clock = pg.time.Clock()
         self.delta_time = 1
         self.map = Map(self)
+        self.player = Player(self)
 
     def update(self):
         self.delta_time = self.clock.tick(FPS)
+        self.player.update()
 
     def draw(self):
         self.screen.fill(color=FIELD_COLOR)
         self.map.draw()
+        self.player.draw()
         pg.display.flip()
 
     def check_events(self):
@@ -35,5 +39,5 @@ class App:
 
 
 if __name__ == '__main__':
-    app = App()
+    app = Game()
     app.run()
